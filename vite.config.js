edf59e6f1025efb8538468af88bab2d04eefbc25
Ghandi-dev <scoutghandi31@gmail.com>
2024-08-16 16:00:24 +0700

    first

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['@emotion/react', '@mui/material', '@mui/styled-engine']
+  }
+});
